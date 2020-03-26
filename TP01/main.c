72e307f106551962741b652719c0389c182a4713
Walid Sa <wals.38100@gmail.com>
2020-03-26 03:49:12 +0100

    Add TP01 ameliored

--- TP01/main.c
+++ TP01/main.c
@@ -4,34 +4,85 @@
 
 int main(int argc, char** argv)
 {
-    const int MAX = 500;
-    const int MIN = 0;
-
     srand(time(NULL));
-    int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+    const int MIN = 0;
+    int max = 0;
+    int nombreMystere = 0;
     int val = 0;
+    int compteur = 0;
+    int again = 1;
+    int difficult = 0;
+    int players = 2;
 
-    printf("%d\n\n", nombreMystere);
-
-    do
+    while (again != 0)
     {
-        printf("Quel est le nombre :? ");
-        scanf("%d", &val);
+        printf("Un ou deux joueurs :? ");
+        scanf("%d", &players);
 
-        if (val < nombreMystere)
+        if (players == 1)
         {
-            printf("C'est plus !\n\n");
-        }
-        else if (val > nombreMystere)
-        {
-            printf("C'est moins !\n\n");
+            printf("\n\n=== Difficulté ===\n");
+            printf("1. 0 - 100\n");
+            printf("2. 0 - 1 000\n");
+            printf("3. 0 - 10 000\n\n");
+
+            printf("Choix de difficulté :? ");
+            scanf("%d", &difficult);
+
+            switch (difficult)
+            {
+            case 1:
+                max = 100;
+                break;
+            case 2:
+                max = 1000;
+                break;
+            case 3:
+                max = 10000;
+                break;
+            default:
+                max = 50000;
+                break;
+            }
+
+            nombreMystere = (rand() % (max - MIN + 1)) + MIN;
         }
         else
         {
-            printf("Bravo, vous avez trouvez (%d)\n", nombreMystere);
+            printf("(J2) Choisir un nombre mystère :? ");
+            scanf("%d", &nombreMystere);
         }
+            printf("%d\n\n", nombreMystere);
 
-    } while (val != nombreMystere);
+        do
+        {
+            compteur++;
+
+            printf("Quel est le nombre :? ");
+            scanf("%d", &val);
+
+            if (val < nombreMystere)
+            {
+                printf("C'est plus !\n\n");
+            }
+            else if (val > nombreMystere)
+            {
+                printf("C'est moins !\n\n");
+            }
+            else
+            {
+                printf("Bravo, vous avez trouvez la bonne réponse (%d) en %d coups!\n\n", nombreMystere, compteur);
+                compteur = 0;
+            }
+
+        } while (val != nombreMystere);
+
+
+        printf("Jouer encore (entrer '0' pour quitter) :? ");
+        scanf("%d", &again);
+
+    }
 
     return 0;
 }
